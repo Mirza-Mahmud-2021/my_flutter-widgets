2c9a78669649f5c7a03088855f4392f0de2a00c0
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-11-18 21:55:12 +0600

    create_dimensions_class

---
+++ lib/components/dimensions.dart
@@ -0,0 +1,13 @@
+class Dimensions{
+
+  /// font-size
+  static const double fontOverSmall = 8.00;
+  static const double fontExtraSmall = 10.00;
+  static const double fontSmall = 12.00;
+  static const double fontDefault = 14.00;
+  static const double fontMedium = 16.00;
+  static const double fontLarge = 18.00;
+  static const double fontExtraLarge = 20.00;
+  static const double fontOverLarge = 20.00;
+  static const double fontHeader = 24.00;
+}
