1ca4e3e13c2e89391b1473677d1c6b411422f98d
Mirza-Mahmud-2021 <mirzamahmud.cse.bubt202@gmail.com>
2022-11-18 21:55:49 +0600

    create_font_styles_all_font_styles_manage_from_here

---
+++ lib/components/font_styles.dart
@@ -0,0 +1,283 @@
+import 'package:flutter/material.dart';
+import 'package:my_flutter_widgets/components/app_color.dart';
+import 'package:my_flutter_widgets/components/dimensions.dart';
+
+/// light-font
+const poppinsLightOverSmall = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontOverSmall,
+    fontWeight: FontWeight.w300
+);
+const poppinsLightExtraSmall = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontExtraSmall,
+    fontWeight: FontWeight.w300
+);
+const poppinsLightSmall = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontSmall,
+    fontWeight: FontWeight.w300
+);
+const poppinsLightDefault = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontDefault,
+    fontWeight: FontWeight.w300
+);
+const poppinsLightSemiBold = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontMedium,
+    fontWeight: FontWeight.w300
+);
+const poppinsLightLarge = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontLarge,
+    fontWeight: FontWeight.w300
+);
+const poppinsLightExtraLarge = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontExtraLarge,
+    fontWeight: FontWeight.w300
+);
+const poppinsLightOverLarge = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontOverLarge,
+    fontWeight: FontWeight.w300
+);
+const poppinsLightHeader = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontHeader,
+    fontWeight: FontWeight.w300
+);
+
+/// regular-font
+const poppinsRegularOverSmall = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontOverSmall,
+    fontWeight: FontWeight.w400
+);
+const poppinsRegularExtraSmall = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontExtraSmall,
+    fontWeight: FontWeight.w400
+);
+const poppinsRegularSmall = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontSmall,
+    fontWeight: FontWeight.w400
+);
+const poppinsRegularDefault = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontDefault,
+    fontWeight: FontWeight.w400
+);
+const poppinsRegularSemiBold = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontMedium,
+    fontWeight: FontWeight.w400
+);
+const poppinsRegularLarge = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontLarge,
+    fontWeight: FontWeight.w400
+);
+const poppinsRegularExtraLarge = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontExtraLarge,
+    fontWeight: FontWeight.w400
+);
+const poppinsRegularOverLarge = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontOverLarge,
+    fontWeight: FontWeight.w400
+);
+const poppinsRegularHeader = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontHeader,
+    fontWeight: FontWeight.w400
+);
+
+/// medium-font
+const poppinsMediumOverSmall = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontOverSmall,
+    fontWeight: FontWeight.w500
+);
+const poppinsMediumExtraSmall = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontExtraSmall,
+    fontWeight: FontWeight.w500
+);
+const poppinsMediumSmall = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontSmall,
+    fontWeight: FontWeight.w500
+);
+const poppinsMediumDefault = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontDefault,
+    fontWeight: FontWeight.w500
+);
+const poppinsMediumMedium = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontMedium,
+    fontWeight: FontWeight.w500
+);
+const poppinsMediumLarge = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontLarge,
+    fontWeight: FontWeight.w500
+);
+const poppinsMediumExtraLarge = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontExtraLarge,
+    fontWeight: FontWeight.w500
+);
+const poppinsMediumOverLarge = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontOverLarge,
+    fontWeight: FontWeight.w500
+);
+const poppinsMediumHeader = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontHeader,
+    fontWeight: FontWeight.w500
+);
+
+/// semi-bold-font
+const poppinsSemiBoldOverSmall = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontOverSmall,
+    fontWeight: FontWeight.w600
+);
+const poppinsSemiBoldExtraSmall = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontExtraSmall,
+    fontWeight: FontWeight.w600
+);
+const poppinsSemiBoldSmall = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontSmall,
+    fontWeight: FontWeight.w600
+);
+const poppinsSemiBoldDefault = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontDefault,
+    fontWeight: FontWeight.w600
+);
+const poppinsSemiBoldSemiBold = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontMedium,
+    fontWeight: FontWeight.w600
+);
+const poppinsSemiBoldLarge = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontLarge,
+    fontWeight: FontWeight.w600
+);
+const poppinsSemiBoldExtraLarge = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontExtraLarge,
+    fontWeight: FontWeight.w600
+);
+const poppinsSemiBoldOverLarge = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontOverLarge,
+    fontWeight: FontWeight.w600
+);
+const poppinsSemiBoldHeader = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontHeader,
+    fontWeight: FontWeight.w600
+);
+
+/// bold-font
+const poppinsBoldOverSmall = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontOverSmall,
+    fontWeight: FontWeight.w700
+);
+const poppinsBoldExtraSmall = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontExtraSmall,
+    fontWeight: FontWeight.w700
+);
+const poppinsBoldSmall = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontSmall,
+    fontWeight: FontWeight.w700
+);
+const poppinsBoldDefault = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontDefault,
+    fontWeight: FontWeight.w700
+);
+const poppinsBoldSemiBold = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontMedium,
+    fontWeight: FontWeight.w700
+);
+const poppinsBoldLarge = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontLarge,
+    fontWeight: FontWeight.w700
+);
+const poppinsBoldExtraLarge = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontExtraLarge,
+    fontWeight: FontWeight.w700
+);
+const poppinsBoldOverLarge = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontOverLarge,
+    fontWeight: FontWeight.w700
+);
+const poppinsBoldHeader = TextStyle(
+    color: AppColor.colorBlack,
+    fontFamily: "Poppins",
+    fontSize: Dimensions.fontHeader,
+    fontWeight: FontWeight.w700
+);
